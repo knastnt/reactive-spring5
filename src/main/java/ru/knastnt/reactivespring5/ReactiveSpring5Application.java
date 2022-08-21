6feefc8dcf310933616b736f71a0865d3e8e368f
knastnt <456kot@mail.ru>
2022-08-21 10:06:39 +1000

    init

---
+++ src/main/java/ru/knastnt/reactivespring5/ReactiveSpring5Application.java
@@ -0,0 +1,13 @@
+package ru.knastnt.reactivespring5;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReactiveSpring5Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReactiveSpring5Application.class, args);
+	}
+
+}
