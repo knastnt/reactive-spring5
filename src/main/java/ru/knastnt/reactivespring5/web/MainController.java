c8b7c1caadd04c82eed2187c4f05419b0de15659
knastnt <456kot@mail.ru>
2022-08-21 10:10:52 +1000

    index

---
+++ src/main/java/ru/knastnt/reactivespring5/web/MainController.java
@@ -0,0 +1,12 @@
+package ru.knastnt.reactivespring5.web;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class MainController {
+    @GetMapping("/")
+    public String root() {
+        return "index";
+    }
+}
